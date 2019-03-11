a2fc3fb29209c8b56d56b9e6f3e15294364e32bc
Juan Fernando Márquez Garcés <32533941+jfernandomarquez@users.noreply.github.com>
2019-03-11 11:05:08 -0500

    Update mkwaldo.pm

--- mkwaldo.pm
+++ mkwaldo.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 #algoritmo para generar el waldo, basado en el algoritmo odlaw(https://github.com/firnsy/barnyard2/blob/master/tools/odlaw)
 use constant { MAX_BUF => 1024};
-my $wal = pack{"Z[".MAX_BUF."] Z[".MAX_BUF."] L L", "/var/log/snort", "snort.log", 1234567890, 0};
+my $wal = pack("Z[".MAX_BUF."] Z[".MAX_BUF."] L L", "/var/log/snort", "snort.log", 1234567890, 0);
 open(WALDO,">barnyard2.waldo");
 print WALDO $wal;
 close WALDO;
